e53105bcb55a08c807d6a8bd0bc607eea3fe665b
Reim-developer <contact.kaxtr@gmail.com>
2025-02-11 13:24:41 +0700

    Added new handles for * hash --file <FILE_PATH>

--- src/cmds/handles.rs
+++ src/cmds/handles.rs
@@ -1,5 +1,6 @@
 use crate::cli::{Cli, Commands};
 use crate::cmds::cat::cat_command;
+use crate::cmds::hash::hash_command;
 use clap::Parser;
 
 pub fn handles_commands() {
@@ -8,6 +9,10 @@ pub fn handles_commands() {
     match args.commands {
         Commands::Cat { file_path } => {
             cat_command(&file_path);
+        }
+        Commands::Hash { file_path, debug } => {
+            hash_command(&file_path);
+            println!("{}", debug);
         }
     }
 }
