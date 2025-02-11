18966691d0ff115560d91bc7734d35cc9978bda4
Reim-developer <contact.kaxtr@gmail.com>
2025-02-12 05:34:17 +0700

    Added * Color * Return type To -> md5 module

--- src/core/md5.rs
+++ src/core/md5.rs
@@ -3,26 +3,24 @@ use md5;
 use memmap2::Mmap;
 use std::fs::File;
 
-pub fn check_md5(file_path: &str) {
+pub fn check_md5(file_path: &str) -> String {
     let error_color = LogLevel::Error.fmt();
     let info_color = LogLevel::Info.fmt();
 
     let file_target = match File::open(file_path) {
         Ok(file) => file,
         Err(error) => {
-            eprintln!("{} Can't read file with error: {}", error_color, error);
-            return;
+            return format!("{} Can't read file with error: {}", error_color, error);
         }
     };
 
     let mmap = match unsafe { Mmap::map(&file_target) } {
         Ok(_mmap) => _mmap,
         Err(error) => {
-            eprintln!("{} Error when memory-mapping file: {}", error_color, error);
-            return;
+            return format!("{} Error when memory-mapping file: {}", error_color, error);
         }
     };
 
     let digest = md5::compute(&mmap);
-    println!("{} MD5: {:?}", info_color, digest);
+    format!("{} MD5: {:?}\n", info_color, digest)
 }
