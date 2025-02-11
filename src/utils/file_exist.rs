1fd0d45afe2f27839448e0ac0aee582c4154d393
Reim-developer <contact.kaxtr@gmail.com>
2025-02-11 13:27:18 +0700

    Added color * ERR -> To log-level

--- src/utils/file_exist.rs
+++ src/utils/file_exist.rs
@@ -1,11 +1,17 @@
+use crate::utils::colors::LogLevel;
 use std::path::Path;
 
 pub fn file_is_exist(file_path: &str) -> bool {
     let file_target = Path::new(file_path);
 
+    let error_color = LogLevel::Error.fmt();
     match file_target.exists() {
         false => {
-            eprintln!("{}: File not found", file_target.to_string_lossy());
+            eprintln!(
+                "{} {}: File not found",
+                error_color,
+                file_target.to_string_lossy()
+            );
             false
         }
         true => true,
