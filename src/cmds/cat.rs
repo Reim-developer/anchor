7c0d7abcdcedf0b7680fc7fc6b09cd0a4180f26f
Reim-developer <contact.kaxtr@gmail.com>
2025-02-12 05:33:30 +0700

    Fixed cat command * New special cal_time function

--- src/cmds/cat.rs
+++ src/cmds/cat.rs
@@ -1,31 +1,38 @@
 use crate::utils::file_exist::file_is_exist;
 use crate::utils::is_dir::is_directory;
+use crate::utils::time::cal_time;
 use memmap2::Mmap;
-use std::{
-    fs::File,
-    io::{self, Write},
-};
+use std::error::Error;
+use std::fs::File;
+use std::thread::sleep;
+use std::time::Duration;
 
-fn mmap_read_file(file_path: &str) -> Result<(), io::Error> {
-    let file_target = File::open(file_path).map_err(|error| {
-        eprintln!("Error when opening file: {}", error);
-        error
-    })?;
+fn mmap_read_file(file_path: &str) -> Result<String, Box<dyn Error>> {
+    let file_target = File::open(file_path)
+        .map_err(|error| Box::<dyn Error>::from(format!("Error when opening file: {}", error)))?;
+
     let mmap = unsafe {
         Mmap::map(&file_target).map_err(|error| {
-            eprintln!("Error when memory-mapping file: {}", error);
-            error
-        })
-    }?;
-    io::stdout().lock().write_all(&mmap)?;
-    Ok(())
+            Box::<dyn Error>::from(format!("Error when memory-mapping file: {}", error))
+        })?
+    };
+    Ok(String::from_utf8_lossy(&mmap).to_string())
 }
 
 pub fn cat_command(file_path: &str) {
     if is_directory(file_path) || !file_is_exist(file_path) {
         return;
     }
-    if let Err(error) = mmap_read_file(file_path) {
-        eprintln!("Found error: {}", error);
-    }
+    cal_time(|| match mmap_read_file(file_path) {
+        Ok(result) => {
+            sleep(Duration::from_millis(500));
+            print!("{}", result);
+            String::new()
+        }
+
+        Err(err) => {
+            eprintln!("Error when reading file {}", err);
+            String::new()
+        }
+    });
 }
