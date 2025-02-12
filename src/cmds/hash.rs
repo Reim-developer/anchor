87bd6f9c06705fa5383b9023c23a7598fbf0e8b1
Reim-developer <contact.kaxtr@gmail.com>
2025-02-12 14:14:16 +0700

    Added new feature * Check sum SHA256 -> To hash command

--- src/cmds/hash.rs
+++ src/cmds/hash.rs
@@ -1,5 +1,6 @@
 use crate::core::hash::md5::check_md5;
 use crate::core::hash::sha1::check_sha1;
+use crate::core::hash::sha256::check_sha256;
 use crate::utils::file::can_read::can_read_file;
 use crate::utils::file::file_exist::file_is_exist;
 use crate::utils::file::is_dir::is_directory;
@@ -18,6 +19,7 @@ pub fn hash_command(file_path: &str) {
         let mut result = String::new();
         result.push_str(&check_md5(file_path));
         result.push_str(&check_sha1(file_path));
+        result.push_str(&check_sha256(file_path));
         result
     });
 }
