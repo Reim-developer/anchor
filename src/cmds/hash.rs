47b68ba75397081300f833b3a834aef770095caa
Reim-developer <contact.kaxtr@gmail.com>
2025-02-12 13:56:24 +0700

    Fixed import file module path

--- src/cmds/hash.rs
+++ src/cmds/hash.rs
@@ -1,9 +1,9 @@
 use crate::core::md5::check_md5;
 use crate::core::sha1::check_sha1;
-use crate::utils::cant_read::can_read_file;
-use crate::utils::file_exist::file_is_exist;
-use crate::utils::is_dir::is_directory;
-use crate::utils::time::cal_time;
+use crate::utils::file::can_read::can_read_file;
+use crate::utils::file::file_exist::file_is_exist;
+use crate::utils::file::is_dir::is_directory;
+use crate::utils::time::time_calc::cal_time;
 
 pub fn hash_command(file_path: &str) {
     if is_directory(file_path) || !file_is_exist(file_path) {
