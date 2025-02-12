882a83d892626fcc4fa3ae53b59cc686da2aa7e5
Reim-developer <contact.kaxtr@gmail.com>
2025-02-13 06:46:58 +0700

    Now error handles will be return as message error

--- src/utils/file/can_read.rs
+++ src/utils/file/can_read.rs
@@ -1,11 +1,11 @@
 use std::fs::File;
-
-pub fn can_read_file(file_path: &str) -> bool {
-    match File::open(file_path).is_ok() {
-        false => {
-            eprintln!("[ERR] Can't read {}: Permission denied", file_path);
-            false
-        }
-        true => true,
+use crate::utils::styles::colors::LogLevel;
+pub fn can_read_file(file_path: &str) -> String {
+    let error_color = LogLevel::Error.fmt();
+    let info_color = LogLevel::Info.fmt();
+    
+    match File::open(file_path) {
+       Ok(_) => format!("{} Checking read permission ... ", info_color),
+        Err(error) => format!("{} Can't read file with error: {}", error_color, error)
     }
 }
