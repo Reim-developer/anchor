0ecd92c144f8e10185aa8cf262d8bde5ba7a4a3d
Reim-developer <contact.kaxtr@gmail.com>
2025-02-12 14:21:36 +0700

    Added new feature sha512 to module list

--- src/cmds/hash.rs
+++ src/cmds/hash.rs
@@ -1,6 +1,7 @@
 use crate::core::hash::md5::check_md5;
 use crate::core::hash::sha1::check_sha1;
 use crate::core::hash::sha256::check_sha256;
+use crate::core::hash::sha512::check_sha512;
 use crate::utils::file::can_read::can_read_file;
 use crate::utils::file::file_exist::file_is_exist;
 use crate::utils::file::is_dir::is_directory;
@@ -20,6 +21,7 @@ pub fn hash_command(file_path: &str) {
         result.push_str(&check_md5(file_path));
         result.push_str(&check_sha1(file_path));
         result.push_str(&check_sha256(file_path));
+        result.push_str(&check_sha512(file_path));
         result
     });
 }
