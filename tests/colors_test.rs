bc668cd34404392fd5b6c441bf4c5e8c66c0a40a
Reim-developer <contact.kaxtr@gmail.com>
2025-02-13 13:56:56 +0700

    Added new color test implements

--- tests/colors_test.rs
+++ tests/colors_test.rs
@@ -6,16 +6,16 @@ use std::io::{self, Write};
 #[test]
 fn colors_test_impl() {
     let mut stdout = io::stdout();
-    match  writeln!(stdout, "ERROR COLOR: {}", LogLevel::Error.fmt()) {
-        Ok(_) => {},
-        Err(error) => eprintln!("Cannot use stdout with error traceback: {}", error)
+    match writeln!(stdout, "ERROR COLOR: {}", LogLevel::Error.fmt()) {
+        Ok(_) => {}
+        Err(error) => eprintln!("Cannot use stdout with error traceback: {}", error),
     }
     match writeln!(stdout, "INFO COLOR: {}", LogLevel::Info.fmt()) {
-        Ok(_) => {},
-        Err(error) => eprintln!("Cannot use stdout with error traceback: {}", error)
+        Ok(_) => {}
+        Err(error) => eprintln!("Cannot use stdout with error traceback: {}", error),
     }
     match writeln!(stdout, "SUCCESS COLOR: {}", LogLevel::Success.fmt()) {
-        Ok(_) => {},
-        Err(error) => eprintln!("Cannot use stdout with error traceback: {}", error)
+        Ok(_) => {}
+        Err(error) => eprintln!("Cannot use stdout with error traceback: {}", error),
     }
 }
