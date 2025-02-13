c1a4103286fff232ee4d0b83748ab86b059c7f8a
Reim-developer <contact.kaxtr@gmail.com>
2025-02-13 12:27:52 +0700

    Added color scheme [SUCESSFULLY] and define this color in fmt() function

--- src/utils/styles/colors.rs
+++ src/utils/styles/colors.rs
@@ -1,15 +1,18 @@
 use owo_colors::OwoColorize;
 
+#[derive(Debug)]
 pub enum LogLevel {
     Info,
     Error,
+    Success
 }
 
 impl LogLevel {
     pub fn fmt(&self) -> String {
         match self {
-            LogLevel::Info => "[INFO]".on_blue().bold().to_string(),
-            LogLevel::Error => "[ERR]".on_red().bold().to_string(),
+            LogLevel::Info => "[INFO]".bright_black().on_bright_cyan().bold().to_string(),
+            LogLevel::Error => "[ERR]".bright_black().on_bright_red().bold().to_string(),
+            LogLevel::Success => "[SUCCESS]".bright_black().on_bright_green().bold().to_string()
         }
     }
 }
