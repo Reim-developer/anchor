0bfe8db46c58be930d937b65d05a3790f7d2ff85
Reim-developer <contact.kaxtr@gmail.com>
2025-02-14 06:22:36 +0700

    Added new Enum define file formats

---
+++ src/utils/formats.rs
@@ -0,0 +1,23 @@
+use super::get_exts::get_file_ext;
+use crate::core::formats::json_fmt::json_formatter;
+
+pub enum FileTypes {
+    Json,
+    Unknow,
+}
+
+impl FileTypes {
+    pub fn where_file(file_path: &str) -> Self {
+        match get_file_ext(file_path).as_ref() {
+            "json" => Self::Json,
+            _ => Self::Unknow,
+        }
+    }
+
+    pub fn format_file(&self, file_path: &str) -> String {
+        match self {
+            Self::Json => json_formatter(file_path),
+            Self::Unknow => format!("Unknow file extension, skipping.."),
+        }
+    }
+}
