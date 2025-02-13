f85d9572e9b7ae183536f3bdcddd9eb3a893b3f2
Reim-developer <contact.kaxtr@gmail.com>
2025-02-13 12:22:42 +0700

    Fixed format! to return String type. And added newline SEMICOLON '\n' to this

--- src/utils/file/can_read.rs
+++ src/utils/file/can_read.rs
@@ -1,11 +1,11 @@
-use std::fs::File;
 use crate::utils::styles::colors::LogLevel;
+use std::fs::File;
 pub fn can_read_file(file_path: &str) -> String {
     let error_color = LogLevel::Error.fmt();
     let info_color = LogLevel::Info.fmt();
-    
+
     match File::open(file_path) {
-       Ok(_) => format!("{} Checking read permission ... ", info_color),
-        Err(error) => format!("{} Can't read file with error: {}", error_color, error)
+        Ok(_) => format!("{} Checking read permission ... Done!\n", info_color),
+        Err(error) => format!("{} Can't read file with error: {}\n", error_color, error),
     }
 }
