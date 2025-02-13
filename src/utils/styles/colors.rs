233a61b0c815c10981740c385f424d4fd125219e
Reim-developer <contact.kaxtr@gmail.com>
2025-02-13 13:56:38 +0700

    Fixed color style in enum define

--- src/utils/styles/colors.rs
+++ src/utils/styles/colors.rs
@@ -4,7 +4,7 @@ use owo_colors::OwoColorize;
 pub enum LogLevel {
     Info,
     Error,
-    Success
+    Success,
 }
 
 impl LogLevel {
@@ -12,7 +12,11 @@ impl LogLevel {
         match self {
             LogLevel::Info => "[INFO]".bright_black().on_bright_cyan().bold().to_string(),
             LogLevel::Error => "[ERR]".bright_black().on_bright_red().bold().to_string(),
-            LogLevel::Success => "[SUCCESS]".bright_black().on_bright_green().bold().to_string()
+            LogLevel::Success => "[SUCCESS]"
+                .bright_black()
+                .on_bright_green()
+                .bold()
+                .to_string(),
         }
     }
 }
