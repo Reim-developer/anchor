38d4ed277503f35bfbed16843b46cc71e5b8a0ee
Reim-developer <contact.kaxtr@gmail.com>
2025-02-15 07:46:41 +0700

    Added new type file support to Enum: XML

--- src/utils/formats.rs
+++ src/utils/formats.rs
@@ -1,8 +1,10 @@
 use super::get_exts::get_file_ext;
 use crate::core::formats::json_fmt::json_formatter;
+use crate::core::formats::xml_fmt::format_xml;
 
 pub enum FileTypes {
     Json,
+    Xml,
     Unknow,
 }
 
@@ -10,6 +12,7 @@ impl FileTypes {
     pub fn where_file(file_path: &str) -> Self {
         match get_file_ext(file_path).as_ref() {
             "json" => Self::Json,
+            "xml" => Self::Xml,
             _ => Self::Unknow,
         }
     }
@@ -17,6 +20,7 @@ impl FileTypes {
     pub fn format_file(&self, file_path: &str) -> String {
         match self {
             Self::Json => json_formatter(file_path),
+            Self::Xml => format_xml(file_path),
             Self::Unknow => format!("Unknow file extension, skipping.."),
         }
     }
